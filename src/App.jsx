7865166b4b48bcd8974f98cbf2d265f8be29fb1f
awwyushh <awwwyushh@gmail.com>
2025-03-11 11:43:46 +0530

    init hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Hero from './Ayush/Hero'
+import Parallex from './Ayush/Parallax'
 
 function App() {
   return (
     <>
       <Hero/>
+      <Parallex/>
     </>
   )
 }
