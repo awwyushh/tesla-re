f122239f8c3f7465729f7ab89ee6c6e593eba235
Rishabhjain610 <jainrishabh2610@gmail.com>
2025-03-11 13:00:55 +0530

    rishabh

--- src/Rishabh.jsx
+++ src/Rishabh.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import SpotlightCard from '../src/rishabh/SpotlightCard';
 import Feature from './rishabh/Feature';
 import Red from './rishabh/Red';
+import Footer from './rishabh/Footer';
 const Rishabh = () => {
   return (
     <div >
     {/* <Feature/> */}
     <Red/>
+    <Footer/>
     </div>
   )
 }
